5fe322d5359492f17876ee6913379f55f2a418f2
Summer WindL <15727575739@163.com>
2018-04-27 10:25:31 +0800

    Create TimeFilter.java

---
+++ com/softetone/springboot/filter/TimeFilter.java
@@ -0,0 +1,43 @@
+package com.softetone.springboot.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 2018年4月26日-下午3:22:01
+ * @author Administrator
+ * TODO
+ */
+//@Component
+public class TimeFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		System.out.println("==========销毁过滤器========");
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		long start = System.currentTimeMillis();
+		
+		arg2.doFilter(arg0, arg1);
+		
+		System.out.println("filter 耗时："+(System.currentTimeMillis()-start));
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		System.out.println("=======初始化过滤器========");
+	}
+
+	
+}
