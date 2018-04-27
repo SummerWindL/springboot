b3688c1172b5c9b9b51b5858b189f66254edfac5
Summer WindL <15727575739@163.com>
2018-04-27 10:28:20 +0800

    Create SpringbootApplication.java

---
+++ com/softetone/springboot/SpringbootApplication.java
@@ -0,0 +1,19 @@
+package com.softetone.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+/**
+ * 2018年4月24日-上午10:22:26
+ * @author Administrator
+ * TODO :
+ * 该注解指定项目为springboot，由此类当作程序入口
+ 	自动装配 web 依赖的环境
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
